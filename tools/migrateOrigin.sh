96c12cb3f3276870700177f5912aee0862c4842b
martin fischbach <martin.fischbach@uni-wuerzburg.de>
2016-11-03 10:01:20 +0100

    touched

--- tools/migrateOrigin.sh
+++ tools/migrateOrigin.sh
@@ -6,5 +6,5 @@ REMOTE_LINE=$(sed 's/^.*\(git@.*\.git\).*$/\1/' <<< $REMOTE_LINE)
 #Replace 'win9148.informatik.uni-wuerzburg.de' with gitlab2.informatik.uni-wuerzburg.de
 #'.'s have to be escaped
 REMOTE=$(sed 's/win9148\.informatik\.uni-wuerzburg\.de/gitlab2\.informatik\.uni-wuerzburg\.de/' <<< $REMOTE_LINE)
-#echo $REMOTE
+#echo $REMOTE 
 git remote set-url origin $REMOTE
