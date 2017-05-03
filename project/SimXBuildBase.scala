3ebc4eb5f1706646d9c8fe425dfc7cf100976741
s232626 <marvin.thaens@stud-mail.uni-wuerzburg.de>
2017-05-03 11:46:22 +0200

    added sbtplugins for fatjar compilation and showing dependency-graph added prototypically implementation to exclude libs for fatjar compilation

--- project/SimXBuildBase.scala
+++ project/SimXBuildBase.scala
@@ -1,10 +1,24 @@
 import sbt._
 import sbt.Keys._
 import sbtunidoc.Plugin.UnidocKeys._
+import sbtassembly.AssemblyPlugin.autoImport._
 
 trait SimXBuildBase extends Build with SimXSettings{
+
+lazy val jarExcludes = Seq(
+	assemblyExcludedJars in assembly := {
+		val cp = (fullClasspath in assembly).value
+			cp filter {name => 	
+			name.data.getName.equals("stax-api-1.0.1.jar") ||
+			name.data.getName.equals("xml-apis-1.4.01.jar") ||
+			name.data.getName.equals("xmlpull-1.1.3.1.jar") ||
+			name.data.getName.equals("xpp3_min-1.1.4c.jar")
+	}}
+)
+
   object SimXComponent {
-    def apply(id: String, base: File) = Project(id, base, settings = buildSettings ++ docSettings)
+    def apply(id: String, base: File) = Project(id, base, settings = buildSettings ++ docSettings
+						++ jarExcludes)
   }
 
   object SimXApplication {
@@ -12,7 +26,7 @@ trait SimXBuildBase extends Build with SimXSettings{
       Project(id, base, settings = buildSettings ++ docSettings ++ Seq(
         setOntoDir := updateWorkingDirectory(baseDirectory.value),
         compile in Compile <<= compile in Compile dependsOn setOntoDir
-      ))
+      ) ++ jarExcludes)
   }
 
   lazy val ontologyGen = Project(
